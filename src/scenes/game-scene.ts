9cdb68a63ac7cd0768074f5231039dcd3edd7627
Shawn Taylor <shawn@mapsandapps.net>
2019-07-06 21:12:14 -0400

    Clean up unused stuff; add title

--- src/scenes/game-scene.ts
+++ src/scenes/game-scene.ts
@@ -1,20 +1,11 @@
-import { CONST } from '../helpers/const'
 import { DebugGraphics } from '../objects/debug-graphics'
 import { Path } from '../objects/path'
 import { Player } from '../objects/player'
 
 export class GameScene extends Phaser.Scene {
-  private joystickStick
-  private joystickStickGraphics
-  private joystickPosition
-  private joystickStickWellGraphics
-
   private debugGraphics: DebugGraphics
   private path: Path
   private player: Player
-  private speedModifier
-  private trajectory
-  private tween
 
   constructor() {
     super({
